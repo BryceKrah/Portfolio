0f45409101f3e08049df7451418dd83684fd0194
Bryce Krah <bkrah@thomasnet.com>
2016-06-28 16:49:15 -0400

    more about section, styling

--- src/containers/about.js
+++ src/containers/about.js
@@ -10,8 +10,15 @@ export default class About extends Component {
         <div className="section">
           <h1 className="section-header">Bryce Krah</h1>
           <div className="about-container">
-            <div>I am a software developer living in Brooklyn, New York. During the day I am interning as a Web Developer for ThomasNet where I collaborate with our design and marketing team to create modern, responsive websites for clients. At night, I work as a Code Advisor for Codecademy.com, providing mentorship to pro members where I review their code, debug issues, and help them get unstuck.</div>
-            <div>When I am not immersed in my coding endeavors, I am usually watching/playing basketball, or wreaking havoc in Halo.</div>
+            <div className="about-item">
+              <div>I am a software developer living in Brooklyn, New York. During the day I am interning as a Web Developer for ThomasNet where I collaborate with our design and marketing team to create modern, responsive websites for clients.</div>
+            </div>
+            <div className="about-item">
+              <div>At night, I work as a Code Advisor for Codecademy.com, providing mentorship to pro members where I review their code, debug issues, and help them get unstuck.</div>
+            </div>
+            <div className="about-item">
+              <div>When I am not immersed in my coding endeavors, I am usually watching/playing basketball, or I'm on my Xbox One wreaking havoc in Halo or whatever the latest RPG game is.</div>
+            </div>
           </div>
         </div>
       </div>
